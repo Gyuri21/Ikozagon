69fdb8e79dc769230b2fb2cdf49765bd5df1f744
Gyuri21 <gyuri21hegedus@gmail.com>
2022-01-06 09:06:59 +0100

    kesz

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'ikozagon';
+  oldal = 45;
+
+  onclickSzamit(){
+    
+
+    let terulet = 5 * Math.pow(this.oldal,2) * (1+Math.sqrt(5)+Math.sqrt(5+2*Math.sqrt(5)));
+    console.log(terulet);
+
+  }
+}
